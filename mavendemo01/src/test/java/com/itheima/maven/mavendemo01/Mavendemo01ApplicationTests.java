0700b92f2af9f1e4eb60e7284a4662ced6d44a3f
Duomuren <rzh534975806@qq.com>
2020-11-28 07:59:47 +0800

    test

---
+++ mavendemo01/src/test/java/com/itheima/maven/mavendemo01/Mavendemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.maven.mavendemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mavendemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
